2e7cea4ea6b65e93f1bb7f1c62e0539b3463144c
Vedran Mandić <mandic.vedran@gmail.com>
2016-08-17 22:50:58 +0200

    first component with webpack-dev-server

--- github-battle/app/index.js
+++ github-battle/app/index.js
@@ -1,2 +1,13 @@
-var app = document.getElementById("app");
-app.innerHTML = "Hello!";
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+var HelloWorld = React.createClass({
+    render: function () {
+        return (<div>Hello World Component!!!</div>);
+    }
+});
+
+ReactDOM.render(
+    <HelloWorld />,
+    document.getElementById("app")
+); 
