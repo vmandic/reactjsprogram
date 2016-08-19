5ace62bda49820e8ec0a213ae31f53eef4b0422c
Vedran Mandić <mandic.vedran@gmail.com>
2016-08-20 00:47:28 +0200

    progress

--- github-battle/app/index.js
+++ github-battle/app/index.js
@@ -1,13 +1,53 @@
+var USER_DATA = {
+    name: 'Vedran Mandic',
+    username: 'vmandic',
+    image: 'https://avatars2.githubusercontent.com/u/1390895?v=3&u=aff79990ddd0351dffba3d9322341a1e2b166ead&s=140'
+}
+
 var React = require("react");
 var ReactDOM = require("react-dom");
 
-var HelloWorld = React.createClass({
+var ProfilePic = React.createClass({
     render: function () {
-        return (<div>Hello World Component!!!</div>);
+        return (
+            <img src={this.props.imageUrl} style={{ height: 100, width: 100 }} />
+        );
+    }
+});
+
+var ProfileLink = React.createClass({
+    render: function () {
+        return (
+            <div>
+                <a href={'https://www.github.com/' + this.props.username}>
+                    {this.props.username}
+                </a>
+            </div>
+        );
+    }
+});
+
+var ProfileName = React.createClass({
+    render: function () {
+        return (
+            <div>{this.props.name}</div>
+        );
+    }
+});
+
+var Avatar = React.createClass({
+    render: function () {
+        return (
+            <div>
+                <ProfilePic imageUrl={this.props.user.image} />
+                <ProfileName name={this.props.user.name} />
+                <ProfileLink username={this.props.user.username} />
+            </div>
+        );
     }
 });
 
 ReactDOM.render(
-    <HelloWorld />,
+    <Avatar user={USER_DATA} />,
     document.getElementById("app")
 ); 
